fdd99543641cd8b095bb30f7fe24c0375ff61114
Anssi Lumme <16646758+anssilumme@users.noreply.github.com>
2023-05-04 19:35:25 +0300

    Changed service account json to be displayed as secret.

--- FrendsGoogleCloudStorage/Definitions/ServiceAccount.cs
+++ FrendsGoogleCloudStorage/Definitions/ServiceAccount.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Text;
 
@@ -14,7 +15,7 @@ namespace FrendsGoogleCloudStorage.Definitions
         /// Service Account JSON.
         /// </summary>
         [Required]
-        [DisplayFormat(DataFormatString = "Json")]
+        [PasswordPropertyText]
         public string ServiceAccountJson { get; set; }
     }
 }
